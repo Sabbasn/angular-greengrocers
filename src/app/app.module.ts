c28383b0af88b301eea8a3b2cb43b4c566092936
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-03-14 14:56:51 +0100

    Refactor page to utilize routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreFrontModule } from './store-front/store-front.module';
+import { Route, RouterModule } from '@angular/router';
+import { StoreComponent } from './store-front/store/store.component';
+import { CartComponent } from './store-front/cart/cart.component';
+
+const appRoutes: Route[] = [
+  { path: '', component: StoreComponent },
+  { path: 'checkout', component: CartComponent },
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StoreFrontModule],
+  imports: [BrowserModule, StoreFrontModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
